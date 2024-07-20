4722a4275373167a36671e96a1d5246cb94683ef
Renni9807 <practiceID98@gmail.com>
2024-07-21 00:14:20 +0800

    Use element component and nest component

--- src/index.js
+++ src/index.js
@@ -1,35 +1,82 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const AmazonPage = () => {
+  return (
+    <>
+      <Header />
+      <BookList />
+    </>
+  );
+};
 
 const BookList = () => {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="books">
+      <BookOne />
+      <BookTwo />
+      <BookThree />
     </section>
   );
 };
 
-const Book = () => (
-  <article>
-    <Image />
-    <Title />
-    <Author />
+const BookOne = () => (
+  <article className="bookOne">
+    <ImageOne />
+    <TitleOne />
+    <AuthorOne />
+  </article>
+);
+const BookTwo = () => (
+  <article className="bookTwo">
+    <ImageTwo />
+    <TitleTwo />
+    <AuthorTwo />
+  </article>
+);
+const BookThree = () => (
+  <article className="bookThree">
+    <ImageThree />
+    <TitleThree />
+    <AuthorThree />
   </article>
 );
 
-const Image = () => <h2>This is image placeholder</h2>;
-const Title = () => {
-  return <h2>Book Title</h2>;
+const Header = () => <h2>Amazon Best Sellers Books</h2>;
+
+const ImageOne = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81ytGlIkLoL._AC_UL900_SR900,600_.jpg"
+    alt="Hillbilly Elegy: A Memoir of a Family and Culture in Crisis"
+  />
+);
+const ImageTwo = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81PIs7jhL0L._AC_UL600_SR600,400_.jpg"
+    alt="Never Lie"
+  />
+);
+const ImageThree = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/611X8GI7hpL._AC_UL600_SR600,400_.jpg"
+    alt="The 48 Laws of Power"
+  />
+);
+
+const TitleOne = () => {
+  return <h2>Hillbilly Elegy: A Memoir of a Family and Culture in Crisis</h2>;
 };
-const Author = () => <h4>Author</h4>;
+const TitleTwo = () => {
+  return <h2>Never Lie</h2>;
+};
+const TitleThree = () => {
+  return <h2>The 48 Laws of Power</h2>;
+};
+const AuthorOne = () => <h4>J. D. Vance</h4>;
+const AuthorTwo = () => <h4>Freida McFadden</h4>;
+const AuthorThree = () => <h4>Robert Greene</h4>;
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<BookList />);
+
+root.render(<AmazonPage />);
